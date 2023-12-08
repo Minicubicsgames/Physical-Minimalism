5bb3760b35efa86eb4b49f3a46a33badb7948aa8
Minicubicsgames <152804917+Minicubicsgames@users.noreply.github.com>
2023-12-08 19:40:36 +0500

    Add files via upload

---
+++ Crackets.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crackets : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        Debug.Log("crickets");
+    }
+}
